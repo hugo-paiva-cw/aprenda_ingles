41cacd5657023e1574af7004c1b54f30665d49d1
Hugo Paiva <hugo.paiva@cloudwalk.io>
2022-05-19 14:44:56 -0300

    Audios added on both animals and numbers tab using AudioCache plugin

--- lib/animals.dart
+++ lib/animals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Animals extends StatefulWidget {
   const Animals({Key? key}) : super(key: key);
@@ -8,6 +9,26 @@ class Animals extends StatefulWidget {
 }
 
 class _AnimalsState extends State<Animals> {
+
+  final AudioCache _audioCache = AudioCache(prefix: 'assets/audios/');
+
+  _executar(String audioName) {
+    _audioCache.play('$audioName.mp3');
+  }
+
+  @override
+  initState() {
+    super.initState();
+    _audioCache.loadAll([
+      'cao.mp3',
+      'gato.mp3',
+      'leao.mp3',
+      'macaco.mp3',
+      'ovelha.mp3',
+      'vaca.mp3',
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return GridView.count(
@@ -17,28 +38,38 @@ class _AnimalsState extends State<Animals> {
       children: [
         GestureDetector(
           onTap: () {
-
+            _executar('cao');
           },
           child: Image.asset('assets/images/cao.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _executar('gato');
+          },
           child: Image.asset('assets/images/gato.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _executar('leao');
+          },
           child: Image.asset('assets/images/leao.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _executar('macaco');
+          },
           child: Image.asset('assets/images/macaco.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _executar('ovelha');
+          },
           child: Image.asset('assets/images/ovelha.png'),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _executar('vaca');
+          },
           child: Image.asset('assets/images/vaca.png'),
         ),
       ],
--- lib/numbers.dart
+++ lib/numbers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Numbers extends StatefulWidget {
   const Numbers({Key? key}) : super(key: key);
@@ -8,6 +9,24 @@ class Numbers extends StatefulWidget {
 }
 
 class _NumbersState extends State<Numbers> {
+  final AudioCache _audioCache = AudioCache(prefix: 'assets/audios/');
+
+  _execute(String audioName) {
+    _audioCache.play('$audioName.mp3');
+  }
+
+  @override
+  initState() {
+    super.initState();
+    _audioCache.loadAll([
+      '1.mp3',
+      '2.mp3',
+      '3.mp3',
+      '4.mp3',
+      '5.mp3',
+      '6.mp3',
+    ]);
+  }
   @override
   Widget build(BuildContext context) {
     return GridView.count(
@@ -15,27 +34,39 @@ class _NumbersState extends State<Numbers> {
         childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
         children: [
           GestureDetector(
-            onTap: () {},
+            onTap: () {
+              _execute('1');
+            },
             child: Image.asset('assets/images/1.png'),
           ),
           GestureDetector(
-            onTap: () {},
+            onTap: () {
+              _execute('2');
+            },
             child: Image.asset('assets/images/2.png'),
           ),
           GestureDetector(
-            onTap: () {},
+            onTap: () {
+              _execute('3');
+            },
             child: Image.asset('assets/images/3.png'),
           ),
           GestureDetector(
-            onTap: () {},
+            onTap: () {
+              _execute('4');
+            },
             child: Image.asset('assets/images/4.png'),
           ),
           GestureDetector(
-            onTap: () {},
+            onTap: () {
+              _execute('5');
+            },
             child: Image.asset('assets/images/5.png'),
           ),
           GestureDetector(
-            onTap: () {},
+            onTap: () {
+              _execute('6');
+            },
             child: Image.asset('assets/images/6.png'),
           ),
         ],
